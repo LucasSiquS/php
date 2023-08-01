d7a6754e6941ea93bcdac0ebd3d2ed4632125341
LucasSiquS <117762661+LucasSiquS@users.noreply.github.com>
2023-08-01 14:43:04 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "Olá mundo!";
+$turma2 = "Vamos desenvolver website em PHP";
+
+// o comando echo mostra as coisas na tela
+
+echo "<br>" .$turma2;
+
+$x = 5 /*dá pra comentar no mei do código kkk*/ + 5;
+echo "<br>". $x;
+
+$txt = "Hello word!";
+$x = 5;
+$y = 10.5;
+echo "<br>" .$txt." x = ".$x." y = ".$y;
+?>
+
+</body>
+</html>
